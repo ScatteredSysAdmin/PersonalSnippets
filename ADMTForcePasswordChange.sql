444e2661aa5ff9f70a97a33b23a536353ceb0a27
ScatteredSysAdmin <59338227+ScatteredSysAdmin@users.noreply.github.com>
2020-01-21 09:50:32 +0100

    Update ADMTForcePasswordChange.sql

--- ADMTForcePasswordChange.sql
+++ ADMTForcePasswordChange.sql
@@ -1,1 +1,10 @@
 -- Show All the Accounts based on where  
+SELECT [ADsPath]
+      ,[SamName]
+      ,[Type]
+	    ,[PasswordCopyTime]
+  FROM [ADMT].[dbo].[Objects] o
+  join [ADMT].[dbo].[MigratedObjects] m on m.SourceObjectId = o.ObjectId
+  where ADSpath LIKE 'SOMETHING'
+  order by ADsPath
+
